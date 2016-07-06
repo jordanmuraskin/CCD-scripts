191555d81290e8b37f2b5949ff5fb0fe69bcbbcf
Jordan Muraskin <jmuraskin@rocky.rfmh.org>
2016-07-06 14:52:18 -0400

    Adding files and pipeline stuff

--- CCD-cpac_filenaming.sh
+++ CCD-cpac_filenaming.sh
@@ -4,23 +4,54 @@
 
 #First start with CCD data in the back up folder
 
-subjects = ls -d /home/jmuraskin/Projects/CCD/data/CCD*/
+#subjects= ls -d /home/jmuraskin/Projects/CCD/data/CCD*/
 
-for subj in $subjects
+for subj in /home/jmuraskin/Projects/CCD/data/CCD*/
 do
 
-mkdir /home/jmuraskin/Projects/CCD/data/${subj}/rest
+	mkdir ${subj}rest
 
-#Copy Anatomy
-for filetype in 1,2
-do
-      fbFile=/home/jmuraskin/Projects/CCD/data/$subj/feedback/fb_${filetype}*
+	#Copy Anatomy
+	for filetype in 1 2;
+	do
+      		fbFile=${subj}feedback/fb_${filetype}*
+	
+
+	        echo "working on ${subj} and file ${filetype}"
 
 
-      echo "working on ${subj} and file ${filetype}"
+      		finalDirectory=${subj}rest/feedback_${filetype}
+      		mkdir ${finalDirectory}
+      		cp -vn ${fbFile} ${finalDirectory}
+	done
+
+	#Copy Anatomy
+        for filetype in 1 2;
+        do
+                fbFile=${subj}peer/peer${filetype}*
 
 
-      finalDirectory=/home/jmuraskin/Projects/CCD/data/${subj}/rest/feedback_${filetype}
-      mkdir ${finalDirectory}
-      cp -vn ${fbFile} ${finalDirectory}
+                echo "working on ${subj} and file ${filetype}"
+
+
+                finalDirectory=${subj}rest/peer_${filetype}
+                mkdir ${finalDirectory}
+                cp -vn ${fbFile} ${finalDirectory}
+        done
+
+	#Copy Anatomy
+        for filetype in 1;
+        do
+                fbFile=${subj}train/train*
+
+
+                echo "working on ${subj} and file ${filetype}"
+
+
+                finalDirectory=${subj}rest/train
+                mkdir ${finalDirectory}
+                cp -vn ${fbFile} ${finalDirectory}
+        done
+
+
 done
