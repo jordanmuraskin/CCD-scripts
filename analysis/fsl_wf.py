46d349d7983fe40dcd895fd9828b91d3880590ab
Jordan <jordan.muraskin@decervo.com>
2016-07-11 11:42:18 -0400

    updated subject list

--- analysis/fsl_wf.py
+++ analysis/fsl_wf.py
@@ -19,7 +19,7 @@ template = '/usr/share/fsl/5.0/data/standard/MNI152_T1_3mm_brain.nii.gz'
 
 
 
-CCD_numbers=[15,17,18,21,23,33,34,40,52,59,60,64,66,74,76,83,89,95]
+CCD_numbers=[15,17,18,21,23,33,40,52,59,64,66,74,76,83,89,95]
 # Specify the subject directories
 
 # subject_list = ['CCD060','CCD066','CCD089']
