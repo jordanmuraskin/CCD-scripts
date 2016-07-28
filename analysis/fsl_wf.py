1b478ea0f8e943acd7e9b37951159514a7b2eb1b
Jordan <jordan.muraskin@decervo.com>
2016-07-28 13:45:05 -0400

    added new subjects

--- analysis/fsl_wf.py
+++ analysis/fsl_wf.py
@@ -20,7 +20,7 @@ template = '/usr/share/fsl/5.0/data/standard/MNI152_T1_3mm_brain.nii.gz'
 
 
 # CCD_numbers=[15,17,18,21,23,33,40,52,59,64,66,74,76,83,89,95]
-CCD_numbers=[15,17,74,89]
+CCD_numbers=[15,17,21,60,74,89,98]
 # Specify the subject directories
 
 # subject_list = ['CCD060','CCD066','CCD089']
